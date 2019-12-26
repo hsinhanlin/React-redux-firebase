9b1af0af7866084969753bf1bb0c3cf0fcbadd8b
Glitch (laced-platypus) <none>
2019-12-26 21:14:45 +0000

    ⏱😅 Checkpoint ./src/store/actions/projectActions.js:9444054/501 ./src/components/projects/CreateProject.js:9444054/493 ./src/store/reducers/projectReducer.js:9444054/144

--- src/components/projects/CreateProject.js
+++ src/components/projects/CreateProject.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import {connect} from 'react-redux'
+import {createProject} from '../../store/actions/projectActions';
 
 class CreateProject extends Component {
   state = {
@@ -14,7 +16,8 @@ class CreateProject extends Component {
 
   handleSubmit = e => {
     e.preventDefault();
-    console.log(this.state);
+    // console.log(this.state);
+    this.props.createProject(this.state);
     e.target.reset();
   };
 
@@ -47,4 +50,10 @@ class CreateProject extends Component {
   }
 }
 
-export default CreateProject;
+const mapDispatchToprops = (dispatch) => {
+  return{
+    createProject: (project) => dispatch(createProject(project))
+  }
+}
+
+export default connect(null, mapDispatchToprops)(CreateProject);
