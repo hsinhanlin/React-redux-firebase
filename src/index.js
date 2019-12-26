cb5cea8438cf9aafea96662e887ffbbcf17e43d3
Glitch (laced-platypus) <none>
2019-12-26 23:01:31 +0000

    🎟👄 Checkpoint ./src/index.js:9444054/2696 ./package.json:9444054/11

--- src/index.js
+++ src/index.js
@@ -1,51 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
-
+import App from "./App.js";
+import * as registerServiceWorker from "./registerServiceWorker";
 import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./store/reducers/rootReducer";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
-import fbConfig from "./config/fbConfig";
-import firebase from 'firebase/app';
-
-import {
-  createFirestoreInstance,
-  reduxFirestore,
-  getFirestore
-} from "redux-firestore";
+import { reduxFirestore, getFirestore } from "redux-firestore";
 import { reactReduxFirebase, getFirebase } from "react-redux-firebase";
-
-import "firebase/firestore";
-
-// const fbConfig = {
-//   userProfile: "projects",
-//   useFirestoreForProfile: true
-// };
+import fbConfig from "./config/fbConfig";
 
 const store = createStore(
   rootReducer,
   compose(
     applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
-    reduxFirestore(fbConfig),
-     reactReduxFirebase(fbConfig)
+    reduxFirestore(fbConfig), // redux binding for firebase
+    reactReduxFirebase(fbConfig, {
+      useFirestoreForProfile: true,
+      userProfile: "users",
+      attachAuthIsReady: true
+    }) // redux bindings for firestore
   )
 );
 
-// const rffProps = {
-//   firebase,
-//   useFirestoreForProfile: true,
-//   config: fbConfig,
-//   dispatch: store.dispatch,
-//   createFirestoreInstance
-// };
-
-ReactDOM.render(
-  <Provider store={store}>
-    
+store.firebaseAuthIsReady.then(() => {
+  ReactDOM.render(
+    <Provider store={store}>
       <App />
-    
-  </Provider>,
-  document.getElementById("root")
-);
+    </Provider>,
+    document.getElementById("root")
+  );
+  registerServiceWorker.unregister();
+});
