f507c2657115e3ae456a799c0980eb5e2c9a9188
Glitch (laced-platypus) <none>
2019-12-26 21:48:59 +0000

    💼🚕 Checkpoint ./src/index.js:9444054/1917 ./src/store/actions/projectActions.js:9444054/2482 ./src/store/reducers/projectReducer.js:9444054/170

--- src/index.js
+++ src/index.js
@@ -6,11 +6,18 @@ import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./store/reducers/rootReducer";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
-import { getFirestore } from "redux-firestore";
-import { getFirebase } from "react-redux-firebase";
+import { reduxFirestore, getFirestore } from "redux-firestore";
+import { reactReduxFirebase, getFirebase } from "react-redux-firebase";
+import fbConfig from './config/fbConfig'
 
-const store = createStore(rootReducer,
-                          compose(applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore}))));
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+    reduxFirestore(fbConfig),
+    reactReduxFirebase(fbConfig)
+  )
+);
 
 ReactDOM.render(
   <Provider store={store}>
--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,6 +1,21 @@
-export const createProject = (project) => {
-  return(dispatch, getState,{getFirebase, getFirestore}) => {
+export const createProject = project => {
+  return (dispatch, getState, { getFirebase, getFirestore }) => {
     // async
-    dispatch({type: 'CREATE_PROJECT', project})
-  }
-} 
+    const firestore = getFirestore();
+    firestore
+      .collection("projects")
+      .add({
+        ...project,
+        authorFirstName: "nana",
+        authorFirstName: "Xx",
+        authorId: 123,
+        createdAt: new Date()
+      })
+      .then(() => {
+        dispatch({ type: "CREATE_PROJECT", project });
+      })
+      .catch(err => {
+        dispatch({type: 'CREATE_PROJECT_ERROR', err});
+      });
+  };
+};
