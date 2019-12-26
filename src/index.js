f5a869ea4101c6b1c5c39a2b830ab02ae36ecc30
Glitch (laced-platypus) <none>
2019-12-26 22:49:32 +0000

    👩‍❤️‍💋‍👩📈 Checkpoint ./src/index.js:9444054/374

--- src/index.js
+++ src/index.js
@@ -7,44 +7,45 @@ import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./store/reducers/rootReducer";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
-import firebase from "./config/fbConfig";
-// import firebase from "firebase/app";
+import fbConfig from "./config/fbConfig";
+import firebase from 'firebase/app';
 
 import {
   createFirestoreInstance,
   reduxFirestore,
   getFirestore
 } from "redux-firestore";
-import { ReactReduxFirebaseProvider, getFirebase } from "react-redux-firebase";
+import { reactReduxFirebase, getFirebase } from "react-redux-firebase";
 
 import "firebase/firestore";
 
-const fbConfig = {
-  userProfile: "projects",
-  useFirestoreForProfile: true
-};
+// const fbConfig = {
+//   userProfile: "projects",
+//   useFirestoreForProfile: true
+// };
 
 const store = createStore(
   rootReducer,
   compose(
     applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
-    reduxFirestore(firebase, fbConfig)
+    reduxFirestore(fbConfig),
+     reactReduxFirebase(fbConfig)
   )
 );
 
-const rffProps = {
-  firebase,
-  useFirestoreForProfile: true,
-  config: fbConfig,
-  dispatch: store.dispatch,
-  createFirestoreInstance
-};
+// const rffProps = {
+//   firebase,
+//   useFirestoreForProfile: true,
+//   config: fbConfig,
+//   dispatch: store.dispatch,
+//   createFirestoreInstance
+// };
 
 ReactDOM.render(
   <Provider store={store}>
-    <ReactReduxFirebaseProvider {...rffProps}>
+    
       <App />
-    </ReactReduxFirebaseProvider>
+    
   </Provider>,
   document.getElementById("root")
 );
