077afcec0a32c66a5b867bdeaa4ec5c39be41a29
Glitch (laced-platypus) <none>
2019-12-26 21:37:46 +0000

    🍨🏢 Checkpoint ./package.json:9444054/3832 ./src/index.js:9444054/1378 ./src/store/actions/projectActions.js:9444054/38

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-import { createStore, applyMiddleware } from 'redux';
-import rootReducer from './store/reducers/rootReducer';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "./store/reducers/rootReducer";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { getFirestore } from "redux-firestore";
+import { getFirebase } from "react-redux-firebase";
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer,
+                          compose(applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore}))));
 
 ReactDOM.render(
-  <Provider store={store}><App /></Provider>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
