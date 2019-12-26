0824c734e4ab4467bdc46540ba1277e54068f05f
Glitch (laced-platypus) <none>
2019-12-26 22:24:30 +0000

    ⚽️🏏 Checkpoint ./src/index.js:9444054/3559 ./package.json:9444054/3980

--- src/index.js
+++ src/index.js
@@ -1,50 +1,49 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
 import "./index.css";
+import App from "./App";
 
 import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./store/reducers/rootReducer";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
-import firebase
-import { createFirestoreInstance, reduxFirestore, getFirestore } from 'redux-firestore'
-import { ReactReduxFirebaseProvider, getFirebase } from 'react-redux-firebase'
+import firebase from "./config/fbConfig";
 
+import {
+  createFirestoreInstance,
+  reduxFirestore,
+  getFirestore
+} from "redux-firestore";
+import { ReactReduxFirebaseProvider, getFirebase } from "react-redux-firebase";
 
-import 'firebase/firestore';
+import "firebase/firestore";
 
+const rrfConfig = {
+  userProfile: "projects",
+  useFirestoreForProfile: true
+};
 
-const rrfConfig = { 
-    userProfile: 'projects',
-    useFirestoreForProfile: true
-}
-
-
-const store = createStore(rootReducer, 
-    compose(
-        applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
-        reduxFirestore(firebase)
-    )
-    );
-
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+    reduxFirestore(firebase, rrfConfig)
+  )
+);
 
 const rffProps = {
-    firebase,
-    useFirestoreForProfile: true,
-    config: rrfConfig,
-    dispatch: store.dispatch,
-    createFirestoreInstance
-}
-
+  firebase,
+  useFirestoreForProfile: true,
+  config: rrfConfig,
+  dispatch: store.dispatch,
+  createFirestoreInstance
+};
 
 ReactDOM.render(
-    <Provider store={store}>
-        <ReactReduxFirebaseProvider {...rffProps}>
-            <App />  
-        </ReactReduxFirebaseProvider>
-    </Provider>, 
-document.getElementById('root'));
-
-
-
+  <Provider store={store}>
+    <ReactReduxFirebaseProvider {...rffProps}>
+      <App />
+    </ReactReduxFirebaseProvider>
+  </Provider>,
+  document.getElementById("root")
+);
