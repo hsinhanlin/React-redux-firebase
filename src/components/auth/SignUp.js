168b958e31a64c51b17d8ddc5e69ac04f9a5e99a
Glitch (laced-platypus) <none>
2019-12-26 17:27:49 +0000

    🐉🙏 Checkpoint ./src/components/auth/SingUp.js:9444054/1517 ./src/App.js:9444054/141 ./src/components/auth/SignUp.js:9444054/4

---
+++ src/components/auth/SignUp.js
@@ -0,0 +1,47 @@
+import React,{Component} from 'react';
+
+class SignUp extends Component{
+  state = {
+    email: '',
+    password: '',
+    firstName: '',
+    lastName: ''
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value
+    })
+  }
+  
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state)
+    e.target.reset();
+  }
+
+  render(){
+    return(
+      <div className="container">
+        <form onSubmit={this.handleSubmit} className="white">
+          <h5 className="grey-text text-darken-3">Sign Up</h5>
+          <div className="input-filed">
+            <label for="email">Email</label>
+            <input type="email" id="email" onChange={this.handleChange}/>
+            <label for="password">Password</label>
+            <input type="password" id="password" onChange={this.handleChange}/>
+              <label for="firstName">First Name</label>
+            <input type="text" id="firstName" onChange={this.handleChange}/>
+                <label for="lastName">Last Nam</label>
+            <input type="text" id="lastName" onChange={this.handleChange}/>
+          </div>
+          <div className="input-field">
+            <button className="btn pink lighten-1 z-depth-0">Sign up</button>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default SignUp
