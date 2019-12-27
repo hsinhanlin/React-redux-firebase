a935f62fbb1af9dfe729d57a92fc918e52a5abf8
Glitch (laced-platypus) <none>
2019-12-27 09:17:31 +0000

    💃🏯 Checkpoint ./src/index.js:9444054/104 ./src/config/fbConfig.js:9444054/221 ./package.json:9444054/2050

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -2,7 +2,7 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 
-const firebaseConfig = {
+const config = {
   apiKey: "AIzaSyDRYsMkj4-vKpzz4-EfasUXjsjcRd1VkfI",
   authDomain: "laced-platypus.firebaseapp.com",
   databaseURL: "https://laced-platypus.firebaseio.com",
@@ -12,7 +12,7 @@ const firebaseConfig = {
   appId: "1:131951710836:web:b0eeaa628ebef214dab721"
 };
 
-firebase.initializeApp(firebaseConfig);
-// firebase.firestore().settings({ timestampsInSnapshots: true });
+firebase.initializeApp(config);
+firebase.firestore().settings({ timestampsInSnapshots: true });
 
 export default firebase;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App.js";
-import * as registerServiceWorker from "./registerServiceWorker";
 import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./store/reducers/rootReducer";
 import { Provider } from "react-redux";
@@ -31,5 +30,4 @@ store.firebaseAuthIsReady.then(() => {
     </Provider>,
     document.getElementById("root")
   );
-  registerServiceWorker.unregister();
 });
