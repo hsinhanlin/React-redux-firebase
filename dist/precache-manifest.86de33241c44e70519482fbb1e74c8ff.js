132138b704f6f3da2fe2c9fe7a469e513ec19888
Glitch (laced-platypus) <none>
2019-12-28 15:51:30 +0100

    update gitignore

---
+++ dist/precache-manifest.86de33241c44e70519482fbb1e74c8ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cd41bb8ac7d8f302f69a0e9bae83311",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef046eb2fcced19f9af9",
+    "url": "/static/css/main.a2e0d47f.chunk.css"
+  },
+  {
+    "revision": "391b08b5e89e10676562",
+    "url": "/static/js/2.de6460a3.chunk.js"
+  },
+  {
+    "revision": "ef046eb2fcced19f9af9",
+    "url": "/static/js/main.947f4db8.chunk.js"
+  },
+  {
+    "revision": "73b3b3e0d5f71a5e9c61",
+    "url": "/static/js/runtime-main.ab42b4fb.js"
+  }
+]);
